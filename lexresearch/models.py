85b9dc6a6b2d08b1affeb5509a948997a4168f61
pv <priyanka@lexres.co.in>
2024-01-23 12:38:36 +0530

    first commit

---
+++ lexresearch/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+class UserProfile(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE ,default=None)
+    # Add any additional fields for user profile data here
+
+  
+
+    def __str__(self):
+        return self.user.username
+
+class UserLogin(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    login_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.login_time}'
